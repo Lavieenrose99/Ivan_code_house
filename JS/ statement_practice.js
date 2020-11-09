c0623bda516b4ef46fe4ceae17b373566ad9f2c5
lavie <895530239@qq.com>
2020-11-09 14:24:16 +0800

    first-commit

--- JS/ statement_practice.js
+++ JS/ statement_practice.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-09 12:35:42
- * @LastEditTime: 2020-11-09 13:24:12
+ * @LastEditTime: 2020-11-09 13:41:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /JS&TS练习代码/JS/ statement_practice.js
