670e6da0c6ab61e7b194e0f8897c23043a1e4658
lavie <895530239@qq.com>
2020-12-07 13:30:02 +0800

    feat: 数组练习

--- JS/Object.js
+++ JS/Object.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 23:53:51
- * @LastEditTime: 2020-12-04 00:04:41
+ * @LastEditTime: 2020-12-07 13:28:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /ivan个人练习代码/JS/Object.js
@@ -14,5 +14,42 @@
  console.log(Obj1['name']===Obj1.name)
  console.log(Obj1[markName])
  //js中还可以用方括号表示法来表示对象属性 优点是可以通过变量来访问属性
-
-
+ console.log(Obj1.valueOf())
+ console.log(Obj1.name.toLocaleString())
+ console.log(Obj1.age.toString())
+ //数组里面
+ const array = ['cjz','lwx','clh','ljh']
+ console.log(array.toString())
+ console.log(array.join('| '))
+ console.log(array.push('lsl'))
+ console.log(array)
+ console.log(array.pop())
+ //头插入
+ console.log(array.unshift('111'))
+ //取出第一个
+ console.log(array.shift())
+ console.log(array.indexOf('clh',3))//第二个参数应该是提升查找效率的
+ console.log(array.lastIndexOf('ljh'))
+ //forEach不会直接改变基本数据类型的值
+array.forEach(function(arr,index,array){
+    return array[index] = 'cjz';
+})
+const objArray = [{id:1,name:'cjz'},{id:2,name:'lwx'},{id:3,name:'clh'},{id:4,name:'ljy'}]
+// objArray.forEach((item,index,arr)=>{
+//     return item.style = '666'
+// })
+objArray.forEach((item,index,arr)=>{
+   arr[index].name  + 'xxx'
+})
+console.log(objArray)
+objArray.forEach((item,index,arr)=>{
+    item.name = arr[index].name  + 'xxx'
+})
+console.log(objArray)
+objArray.forEach((item,index,arr)=>{
+    console.log(item===arr[index])
+})
+//console.log(array)
+console.log(array.reduce((pre,aft)=>{
+     return pre + aft
+}))
---
+++ algorithms/sort.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-04 00:14:00
+ * @LastEditTime: 2020-12-04 00:14:00
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /ivan个人练习代码/algorithms/sort.js
+ */
